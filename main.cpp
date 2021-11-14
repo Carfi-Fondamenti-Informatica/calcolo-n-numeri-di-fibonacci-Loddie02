6c474c26b302fc2d993e5d84e1035eced0c7cbef
Loddie02 <91880336+Loddie02@users.noreply.github.com>
2021-11-15 00:57:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 int main () {
-    int n=0, b=1, a=0, c=0;
-        cin>>n;
-        
-        cout<<b<<endl;
-           for(c=0;c<n;c++) {
-                c=a+b;
-                cout<<c<<endl;
-                a=b;
-                b=c;
-            }
-    return 0;
-}
+    int i=0, n=0, primo =1, secondo=1, terzo=0;
+    if (n<2) {
+        cout<<"errore"<<endl;
+    } else {
+        cout<<primo<<endl;
+        cout<<secondo<<endl;
+    for (int i=2; i<n; i++) {
+        terzo=primo+secondo;
+        primo=secondo;
+        secondo=terzo;
+        cout<<terzo<<endl;
+    }
+     
+   
